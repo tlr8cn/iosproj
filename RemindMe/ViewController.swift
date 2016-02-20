d0b9f8f210e8eeb015717c3972acc50187d9a84a
tlr8cn <tlr8cn@virginia.edu>
2016-02-19 17:42:32 -0800

    initial commit

---
+++ RemindMe/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  RemindMe
+//
+//  Created by Tyler Robinson on 2/19/16.
+//  Copyright (c) 2016 Tyler Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+
+}
+
